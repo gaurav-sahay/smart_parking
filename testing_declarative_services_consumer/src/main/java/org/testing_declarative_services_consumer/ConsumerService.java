bc74395b06edb348ef743a2cf3a482ffdcc42b1b
Gaurav <gaurav.sahay@in.bosch.com>
2016-12-07 20:12:05 +0530

    Implemented the pax logging. Steps required to configure pax logging in the felix: Add the following dependency in the pom to use the logging
    <dependency>
    			<groupId>org.ops4j.pax.logging</groupId>
    			<artifactId>pax-logging-service</artifactId>
    			<version>1.9.1</version>
    		</dependency>
    
    
    
    
    
    
    install following bundles in the felix container:
    pax-logging-service-1.9.1
    pax-logging-api-1.9.1
    pax-confman-propsloader-0.2.3
    
    create the configurations folder under the installation directory of the
    felix.
    create services and factories folder under the configurations folder.
    put the org.ops4j.pax.logging.properties file under the services folder
    with all the necessary content for the logger(put the log4j properties
    configurations content in the org.ops4j.pax.logging.properties file)
    
    Note: To enable the logging for all the bundles make sure to install the
    pax logging bundles in the starting itself before even installing any
    other bundles.

--- testing_declarative_services_consumer/src/main/java/org/testing_declarative_services_consumer/ConsumerService.java
+++ testing_declarative_services_consumer/src/main/java/org/testing_declarative_services_consumer/ConsumerService.java
@@ -3,25 +3,27 @@ package org.testing_declarative_services_consumer;
 import org.apache.felix.scr.annotations.Component;
 import org.apache.felix.scr.annotations.Property;
 import org.apache.felix.scr.annotations.Reference;
+import org.apache.log4j.Logger;
 import org.testing_declarative_service_api.FirstServiceApi;
 
 @Component(name="Declarative Consumer Service")
 public class ConsumerService {
+	private static final Logger  LOG = Logger.getLogger(ConsumerService.class);
 	@Property(name="consumer service")
 	@Reference(bind="setFirstService",unbind="unsetFirstService",referenceInterface=FirstServiceApi.class)
 	private FirstServiceApi firstServiceApi;
 	
 	protected void setFirstService(FirstServiceApi firstServiceApi){
 		this.firstServiceApi = firstServiceApi;
-		System.out.println("Setting the service ");
+		LOG.info("Setting the first service");
 		this.firstServiceApi.sayHello();
 	}
 	
 	protected void unsetFirstService(FirstServiceApi firstServiceApi){
+		LOG.info("Unsetting the first service");
 		this.firstServiceApi.sayByeBye();
 		this.firstServiceApi =  null;
 	}
-	
 	
 
 }
--- testing_declarative_services_consumer/src/main/java/org/testing_declarative_services_consumer/TestServletService.java
+++ testing_declarative_services_consumer/src/main/java/org/testing_declarative_services_consumer/TestServletService.java
@@ -7,12 +7,14 @@ import javax.servlet.ServletException;
 import org.apache.felix.scr.annotations.Component;
 import org.apache.felix.scr.annotations.Property;
 import org.apache.felix.scr.annotations.Reference;
+import org.apache.log4j.Logger;
 import org.glassfish.jersey.servlet.ServletContainer;
 import org.osgi.service.http.HttpService;
 import org.osgi.service.http.NamespaceException;
 
 @Component(name="Test Servlet Consumer Service")
 public class TestServletService {
+	private static final Logger  LOG = Logger.getLogger(TestServletService.class);
 	
 	@Property(name="test servlet consumer service")
 	@Reference(bind="setHttpService",unbind="unsetHttpService",referenceInterface=HttpService.class)
@@ -20,13 +22,14 @@ public class TestServletService {
 	
 	protected void setHttpService(HttpService httpService) throws ServletException, NamespaceException{
 		this.httpService = httpService;
-		System.out.println("Setting the httpservice ");
+		LOG.info("Setting the httpservice");
 		Hashtable<String, String> props = new Hashtable<String, String>();
 		props.put("jersey.config.server.provider.packages", "org.smart.parking.raspi.driver.webService");
 		httpService.registerServlet("/hello/*", new ServletContainer(), props, null);
 	}
 	
 	protected void unsetHttpService(HttpService httpService){
+		LOG.info("Unsetting the httpservice");
 		httpService.unregister("/hello/*");
 		this.httpService =  null;
 	}
